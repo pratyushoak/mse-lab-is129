744c818459e4e80be445c8154f8785719e99bba4
pratyushoak <pratyushoak@gmail.com>
2013-10-02 16:09:47 +0530

    rel

--- lab-3/main.m
+++ lab-3/main.m
@@ -3,9 +3,9 @@
 int main(int argc, char* argv[])
 {
 	Employee *emp=[[Employee alloc]init];
-	[emp setName:@"ashish"];
+	[emp setName:@"Pratyush"];
 	[emp setDept:@"ISE"];
-	[emp setEid:132];
+	[emp setEid:129];
 	NSLog(@"Name is %@",[emp getName]);
 	NSLog(@"Dept is %@",[emp getDept]);
 	NSLog(@"EID: %d",[emp getEid]);
